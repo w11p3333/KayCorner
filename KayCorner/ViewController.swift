7fc662a30c9194ea3d4066337c2c11974f532e1b
llx <535921901@qq.com>
2016-04-15 14:10:22 +0800

    Initial Commit

---
+++ KayCorner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KayCorner
+//
+//  Created by 卢良潇 on 16/4/15.
+//  Copyright © 2016年 卢良潇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
